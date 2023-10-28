2a8903a66283e9898c5354c81cfbe56fa646f565
rei-canodemaj <reicanodemaj1@gmail.com>
2023-10-29 01:05:28 +0200

    creating the initial outline of the web app

---
+++ Art Gallery/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/constituents")
+def constituents_page():
+    return render_template("constituents.html")
+
+@app.route("/objects")
+def objects_page():
+    return render_template("objects.html")
+
+@app.route("/locations")
+def locations_page():
+    return render_template("locations.html")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8888, debug=True)
