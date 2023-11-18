a7c7b15e6f249ac6eed721a966eee44e55799eb0
rei-canodemaj <reicanodemaj1@gmail.com>
2023-11-18 19:18:42 +0100

    updating views.py (adding functions to get data from both tables in objects_page)

--- Art Gallery/views.py
+++ Art Gallery/views.py
@@ -21,8 +21,28 @@ def get_objects_constituents():
         return results
     
 def constituents_page():
-    if request.method == "GET":
         constituents = get_constituents()
         objects_constituents = get_objects_constituents()
         return render_template("constituents.html", const=constituents, obj_const=objects_constituents)
       
+def get_objects():
+    conn = sqlite3.connect('./static/database_objects.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM OBJECTS")
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+def get_objects_historical_data():
+        conn = sqlite3.connect('./static/database_objects.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM OBJECTS_HISORICAL_DATA")
+        results = cursor.fetchall()
+        conn.close()
+        return results
+    
+def objects_page():
+        objects = get_objects()
+        objects_historical_data = get_objects_historical_data()
+        return render_template("objects.html", obj=objects, obj_hist=objects_historical_data)
+   
